1d6cb1f32d69948fa6cf06775da96d7d52599bca
Ueli Kunz <kunz@ideadapt.net>
2025-01-02 07:58:08 +0100

    split tests

--- worker/src/test/kotlin/net/ideadapt/gagmap/WorkerKtTest.kt
+++ worker/src/test/kotlin/net/ideadapt/gagmap/WorkerKtTest.kt
@@ -7,22 +7,11 @@ import org.junit.jupiter.api.Test
 class WorkerKtTest {
 
     @Test
-    fun extractTemporalRefs() {
+    fun `extractTemporalRefs day`() {
         val refs = extractTemporalRefs(
             """
         lorem 14. Januar 2022 ipsum
         lorem 4. Februar 2022 ipsum
-        lorem Oktober 2023 ipsum
-        lorem 19. Jahrhundert vdZw ipsum
-        lorem 19. Jahrhundert ipsum
-        lorem 1. Jahrhundert ipsum
-        lorem Jahr 2022 vdZw ipsum
-        lorem Jahr 2022 ipsum
-        lorem 2000er Jahre ipsum
-        lorem 80er Jahre ipsum -- wird als 1980 behandelt
-        lorem 80er Jahre vdZw ipsum -- wird als -89 - -80 behandelt
-        lorem 0er Jahre ipsum
-        lorem 0er Jahre vdZw ipsum
    """
         )
 
@@ -30,38 +19,85 @@ class WorkerKtTest {
         assertEquals(Instant.parse("2022-01-14T00:00:00Z"), refs[0].start)
         assertEquals(Instant.parse("2022-01-14T23:59:59Z"), refs[0].end)
         assertEquals("4. Februar 2022", refs[1].literal)
-        assertEquals("Oktober 2023", refs[2].literal)
-        assertEquals(Instant.parse("2023-10-01T00:00:00Z"), refs[2].start)
-        assertEquals(Instant.parse("2023-10-01T23:59:59Z"), refs[2].end)
-            assertEquals("19. Jahrhundert vdZw", refs[3].literal)
-        assertEquals(Instant.parse("-1900-01-01T00:00:00Z"), refs[3].start)
-        assertEquals(Instant.parse("-1801-12-31T23:59:59Z"), refs[3].end)
-            assertEquals("19. Jahrhundert", refs[4].literal)
-        assertEquals(Instant.parse("1801-01-01T00:00:00Z"), refs[4].start)
-        assertEquals(Instant.parse("1900-12-31T23:59:59Z"), refs[4].end)
-            assertEquals("1. Jahrhundert", refs[5].literal)
-        assertEquals(Instant.parse("0001-01-01T00:00:00Z"), refs[5].start)
-        assertEquals(Instant.parse("0100-12-31T23:59:59Z"), refs[5].end)
-            assertEquals("Jahr 2022 vdZw", refs[6].literal)
-            assertEquals(Instant.parse("-2022-01-01T00:00:00Z"), refs[6].start)
-            assertEquals(Instant.parse("-2022-12-31T23:59:59Z"), refs[6].end)
-            assertEquals("Jahr 2022", refs[7].literal)
-            assertEquals(Instant.parse("2022-01-01T00:00:00Z"), refs[7].start)
-            assertEquals(Instant.parse("2022-12-31T23:59:59Z"), refs[7].end)
-        assertEquals("2000er Jahre", refs[8].literal)
-            assertEquals(Instant.parse("2000-01-01T00:00:00Z"), refs[8].start)
-            assertEquals(Instant.parse("2009-12-31T23:59:59Z"), refs[8].end)
-            assertEquals("80er Jahre", refs[9].literal)
-            assertEquals(Instant.parse("1980-01-01T00:00:00Z"), refs[9].start)
-            assertEquals(Instant.parse("1989-12-31T23:59:59Z"), refs[9].end)
-            assertEquals("80er Jahre vdZw", refs[10].literal)
-            assertEquals(Instant.parse("-0089-01-01T00:00:00Z"), refs[10].start)
-            assertEquals(Instant.parse("-0080-12-31T23:59:59Z"), refs[10].end)
-            assertEquals("0er Jahre", refs[11].literal)
-            assertEquals(Instant.parse("0001-01-01T00:00:00Z"), refs[11].start)
-            assertEquals(Instant.parse("0009-12-31T23:59:59Z"), refs[11].end)
-            assertEquals("0er Jahre vdZw", refs[12].literal)
-            assertEquals(Instant.parse("-0009-01-01T00:00:00Z"), refs[12].start)
-            assertEquals(Instant.parse("-0001-12-31T23:59:59Z"), refs[12].end)
+    }
+
+    @Test
+    fun `extractTemporalRefs month`() {
+        val refs = extractTemporalRefs(
+            """
+        lorem Oktober 2023 ipsum
+   """
+        )
+
+        assertEquals("Oktober 2023", refs[0].literal)
+        assertEquals(Instant.parse("2023-10-01T00:00:00Z"), refs[0].start)
+        assertEquals(Instant.parse("2023-10-01T23:59:59Z"), refs[0].end)
+    }
+
+    @Test
+    fun `extractTemporalRefs Jahrhundert`() {
+        val refs = extractTemporalRefs(
+            """
+        lorem 19. Jahrhundert vdZw ipsum
+        lorem 19. Jahrhundert ipsum
+        lorem 1. Jahrhundert ipsum
+   """
+        )
+
+        assertEquals("19. Jahrhundert vdZw", refs[0].literal)
+        assertEquals(Instant.parse("-1900-01-01T00:00:00Z"), refs[0].start)
+        assertEquals(Instant.parse("-1801-12-31T23:59:59Z"), refs[0].end)
+        assertEquals("19. Jahrhundert", refs[1].literal)
+        assertEquals(Instant.parse("1801-01-01T00:00:00Z"), refs[1].start)
+        assertEquals(Instant.parse("1900-12-31T23:59:59Z"), refs[1].end)
+        assertEquals("1. Jahrhundert", refs[2].literal)
+        assertEquals(Instant.parse("0001-01-01T00:00:00Z"), refs[2].start)
+        assertEquals(Instant.parse("0100-12-31T23:59:59Z"), refs[2].end)
+    }
+
+    @Test
+    fun `extractTemporalRefs year`() {
+        val refs = extractTemporalRefs(
+            """
+        lorem Jahr 2022 vdZw ipsum
+        lorem Jahr 2022 ipsum
+   """
+        )
+
+        assertEquals("Jahr 2022 vdZw", refs[0].literal)
+        assertEquals(Instant.parse("-2022-01-01T00:00:00Z"), refs[0].start)
+        assertEquals(Instant.parse("-2022-12-31T23:59:59Z"), refs[0].end)
+        assertEquals("Jahr 2022", refs[1].literal)
+        assertEquals(Instant.parse("2022-01-01T00:00:00Z"), refs[1].start)
+        assertEquals(Instant.parse("2022-12-31T23:59:59Z"), refs[1].end)
+    }
+
+    @Test
+    fun `extractTemporalRefs Jahrzehnt bzw Dekade absolute`() {
+        val refs = extractTemporalRefs(
+            """
+        lorem 2000er Jahre ipsum
+        lorem 1980er Jahren ipsum
+        lorem 1980er Jahre vdZw ipsum
+        lorem 0er Jahre ipsum
+        lorem 0er Jahre vdZw ipsum
+   """
+        )
+
+        assertEquals("2000er Jahre", refs[0].literal)
+        assertEquals(Instant.parse("2000-01-01T00:00:00Z"), refs[0].start)
+        assertEquals(Instant.parse("2009-12-31T23:59:59Z"), refs[0].end)
+        assertEquals("1980er Jahren", refs[1].literal)
+        assertEquals(Instant.parse("1980-01-01T00:00:00Z"), refs[1].start)
+        assertEquals(Instant.parse("1989-12-31T23:59:59Z"), refs[1].end)
+        assertEquals("1980er Jahre vdZw", refs[2].literal)
+        assertEquals(Instant.parse("-1989-01-01T00:00:00Z"), refs[2].start)
+        assertEquals(Instant.parse("-1980-12-31T23:59:59Z"), refs[2].end)
+        assertEquals("0er Jahre", refs[3].literal)
+        assertEquals(Instant.parse("0001-01-01T00:00:00Z"), refs[3].start)
+        assertEquals(Instant.parse("0009-12-31T23:59:59Z"), refs[3].end)
+        assertEquals("0er Jahre vdZw", refs[4].literal)
+        assertEquals(Instant.parse("-0009-01-01T00:00:00Z"), refs[4].start)
+        assertEquals(Instant.parse("-0001-12-31T23:59:59Z"), refs[4].end)
     }
 }
